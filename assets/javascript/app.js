457ea8c0a15eb9269ceb998f1d38187d03f01fce
Scripted Well <whigareda@theblackpilledpage.com>
2018-12-03 18:53:27 -0700

    Finished index.html & app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,34 +11,60 @@ $(document).ready(function () {
     console.log(firebase);
 
     var database = firebase.database();
+    //var ref = database.ref();
 
     //onClick event variables
-    const name;
-    const destination;
-    const firstTrain;
-    const frequency = 0;
+    var name;
+    var destination;
+    var firstTrain;
+    var frequency = 0;
 
     //onClick Event
-    $("#add-train").on("click", function() {
-        empty(); //clear variables
+    $("#add-train").on("click", function () {
+        event.preventDefault(); //clear variables
         name = $("#train-name").val().trim();
         destination = $("#destination").val().trim();
         firstTrain = $("#first-train").val().trim();
         frequency = $("#frequency").val().trim();
 
         //Push to firebase
-        database.ref().push({
+        database.ref('trains').push({
             name: name,
             destination: destination,
             firstTrain: firstTrain,
-            frequency: frequency,
-            dateAdded: firebase.database.Servervalue.TIMESTAMP
+            frequency: frequency
         });
         $("form")[0].reset();
     });
+
     //Database reference to added child
+    database.ref('trains').on("child_added", function (childSnapshot) {
+        console.log(childSnapshot)
+        var nextArr;
+        var minAway;
+        //change year for first train
+        var firstTrainNew = moment(childSnapshot.val().firstTrain, "hh:mm").subtract(1, "years");
+        //Diff for current and first train
+        var diffTime = moment().diff(moment(firstTrainNew), "minutes");
+        var remainder = diffTime % childSnapshot.val().frequency;
+        //Minutes until the arrival of the next train
+        var minAway = childSnapshot.val().frequency - remainder;
+        //Next train arrival time
+        var nextTrain = moment().add(minAway, "minutes");
+        nextTrain = moment(nextTrain).format("hh:mm");
 
-    //Database refernce to order by Child
+        $("#add-row").append(`<tr>
+        <td> ${childSnapshot.val().name} </td>
+        <td> ${childSnapshot.val().destination} </td>
+        <td> ${childSnapshot.val().frequency} </td>
+        <td> ${nextTrain} </td>
+        <td> ${minAway} </td>
+        </tr>`);
 
+        //To handle errors    
+    }, function (errorObject) {
+        console.log("Errors handled: " + errorObject.code);
+
+    });
 
 });
